15ccfc848f89ffbe5cc1073c6249c093202d7831
mokokoane <malepamk@gmail.com>
2021-08-26 10:31:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Game from './components/Game';
+
+function App() {
+  return (
+    <div className="App">
+        <p>Tic tac toe</p>
+        <Game/>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Board.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import Square from "./Square";
+import './Board.css'
+
+
+interface BoardProps {
+    squares: string[];
+    boardClick: (index: number) => void;
+   
+}
+
+export default function Board({ boardClick, squares }: BoardProps) {
+     
+    return (
+    <div>
+      <div className="board">
+        <div className="row">
+          <Square value={squares[0]} index={0} clickListener={boardClick} />
+          <Square value={squares[1]} index={1} clickListener={boardClick} />
+          <Square value={squares[2]} index={2} clickListener={boardClick} />
+        </div>
+
+        <div className="row">
+          <Square value={squares[3]} index={3} clickListener={boardClick} />
+          <Square value={squares[4]} index={4} clickListener={boardClick} />
+          <Square value={squares[5]} index={5} clickListener={boardClick} />
+        </div>
+
+        <div className="row">
+          <Square value={squares[6]} index={6} clickListener={boardClick} />
+          <Square value={squares[7]} index={7} clickListener={boardClick} />
+          <Square value={squares[8]} index={8} clickListener={boardClick} />
+        </div>
+      </div>
+    </div>
+  );
+}
